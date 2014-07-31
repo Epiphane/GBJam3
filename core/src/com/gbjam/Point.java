365e275833742ca29717f973f1f6c6eff3d2916d
Thomas Steinke <exyphnos@gmail.com>
2014-07-30 19:05:01 -0700

    Adding some basic classes

---
+++ core/src/com/gbjam/Point.java
@@ -0,0 +1,5 @@
+package com.gbjam;
+
+public class Point {
+
+}
--- desktop/src/com/gbjam/desktop/DesktopLauncher.java
+++ desktop/src/com/gbjam/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.gbjam.GBJam3;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 160;
+		config.height = 144;
 		new LwjglApplication(new GBJam3(), config);
 	}
 }
