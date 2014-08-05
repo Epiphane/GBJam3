26836e3a733a45ff3445a9fe6dc615021e2a0902
Elliot Fiske <elliotfiske@gmail.com>
2014-08-04 21:36:56 -0700

    Implementing graphics component, texture splitting utility

--- core/src/com/gbjam/game_components/GraphicsComponent.java
+++ core/src/com/gbjam/game_components/GraphicsComponent.java
@@ -1,17 +1,23 @@
 package com.gbjam.game_components;
 
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.gbjam.Entity;
 import com.gbjam.resource_mgmt.GraphicsService;
 
 public class GraphicsComponent {
-	private Texture texture;
+	private TextureRegion[][] texture;
+	/** Which frame the sprite is on. */
+	private int frame = 0;
+	/** Which 'state' the sprite is in - changes on direction change / hurt or not, etc */
+	private int state = 0;
+	private int ticksSinceLastFrame = 0;
 	
-	public GraphicsComponent(Texture _texture) {
+	public GraphicsComponent(TextureRegion[][] _texture) {
 		texture = _texture;
 	}
 	
 	public void render(Entity object) {
-		GraphicsService.draw(texture, object.getX(), object.getY());
+		GraphicsService.draw(texture[frame][state], object.getX(), object.getY());
+		ticksSinceLastFrame++;
 	}
 }
