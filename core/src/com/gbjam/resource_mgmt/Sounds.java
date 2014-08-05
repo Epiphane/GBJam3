0a3524fa4df41b9ed531195e6389d79b85386c77
Thomas Steinke <exyphnos@gmail.com>
2014-08-04 22:43:29 -0700

    THE HILLS ARE ALIVE

--- core/src/com/gbjam/resource_mgmt/Sounds.java
+++ core/src/com/gbjam/resource_mgmt/Sounds.java
@@ -2,8 +2,18 @@ package com.gbjam.resource_mgmt;
 
 import java.io.IOException;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
 public class Sounds {
+	static private Sound gunSound;
+	
 	static public void load() throws IOException {
-		
+		gunSound = Gdx.audio.newSound(Gdx.files.internal("sounds/gun.mp3"));
+		//gunSound.loop();
+	}
+	
+	static public void playSound(int soundID) {
+		gunSound.loop();
 	}
 }
