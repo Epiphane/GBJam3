a0d968b78337301d07300853d2e20e1c5ead562d
Thomas Steinke <exyphnos@gmail.com>
2014-08-08 09:24:15 -0700

    That wasn't so baaaaad

--- core/src/com/gbjam/game_components/collision/BulletCollisionComponent.java
+++ core/src/com/gbjam/game_components/collision/BulletCollisionComponent.java
@@ -1,6 +1,16 @@
 package com.gbjam.game_components.collision;
 
-import com.gbjam.game_components.collision.CollisionComponent.ColliderType;
+import com.gbjam.Entity;
 
-public class BulletCollisionComponent extends CollisionComponent {
+public class BulletCollisionComponent extends DynamicCollisionComponent {
+	
+	public void init(ColliderType type_) {
+		super.init(type_);
+		filter[ColliderType.ENEMY.ordinal()] = true;
+	}
+	
+	protected void collideAnyDir(Entity me, Entity collider) {
+		collider.setDead(true);
+		me.setDead(true);
+	}
 }
