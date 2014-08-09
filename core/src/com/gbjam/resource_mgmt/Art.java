3dd11fd6d6d5e2b3f37980235b24cf386499cc45
Elliot Fiske <elliotfiske@gmail.com>
2014-08-08 18:40:33 -0700

    FIX HTML5 NOW AND FOREVER

--- core/src/com/gbjam/resource_mgmt/Art.java
+++ core/src/com/gbjam/resource_mgmt/Art.java
@@ -13,7 +13,6 @@ public class Art {
 	static public void load() throws IOException {
 		character = new AnimationSheet("knight.png", 1, 1);
 		slime = new AnimationSheet("slime.png", 1, 1);
-		Gdx.app.log("AssetPath", Gdx.files.internal("slime.png").file().getAbsolutePath());
 		bullet = new AnimationSheet("bullet.png", 1);
 		platform = new AnimationSheet("platform.png", 1);
 		
