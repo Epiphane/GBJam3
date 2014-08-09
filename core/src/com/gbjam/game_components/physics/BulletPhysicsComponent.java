64fcdfd7a7f832f265d8cbaf5d625fb94d0e5d5c
Elliot Fiske <elliotfiske@gmail.com>
2014-08-08 19:57:31 -0700

    Fire starter... ing

--- core/src/com/gbjam/game_components/physics/BulletPhysicsComponent.java
+++ core/src/com/gbjam/game_components/physics/BulletPhysicsComponent.java
@@ -7,6 +7,7 @@ import com.gbjam.game_components.status.StatusComponent.StatusType;
 public class BulletPhysicsComponent extends PhysicsComponent {
 	public void update(Entity object) {
 		object.setX(object.getX() + object.getDX());
+		object.setY(object.getY() + object.getDY());
 		if(object.getX() + object.getPolygon().getBoundingRectangle().width < 0 || object.getX() > GBJam3.GAME_WIDTH) {
 			object.setStatus(StatusType.DEAD, true);
 		}
